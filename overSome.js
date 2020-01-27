1bf15b27997f660b1e770b4ebc4188346d7a64ee
Lolavine777 <60313682+Lolavine777@users.noreply.github.com>
2020-01-27 16:23:07 +0700

    Add files via upload

--- overSome.js
+++ overSome.js
@@ -1,5 +1,30 @@
-var convert = require('./convert'),
-    func = convert('overSome', require('../overSome'));
+var arraySome = require('./_arraySome'),
+    createOver = require('./_createOver');
 
-func.placeholder = require('./placeholder');
-module.exports = func;
+/**
+ * Creates a function that checks if **any** of the `predicates` return
+ * truthy when invoked with the arguments it receives.
+ *
+ * @static
+ * @memberOf _
+ * @since 4.0.0
+ * @category Util
+ * @param {...(Function|Function[])} [predicates=[_.identity]]
+ *  The predicates to check.
+ * @returns {Function} Returns the new function.
+ * @example
+ *
+ * var func = _.overSome([Boolean, isFinite]);
+ *
+ * func('1');
+ * // => true
+ *
+ * func(null);
+ * // => true
+ *
+ * func(NaN);
+ * // => false
+ */
+var overSome = createOver(arraySome);
+
+module.exports = overSome;
